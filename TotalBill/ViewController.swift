e05863b468b5f661c8824c2ab007eba8f1a58c89
justSmK <28645140+justSmK@users.noreply.github.com>
2023-08-08 19:44:10 +0300

    Project init

---
+++ TotalBill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TotalBill
+//
+//  Created by Sergei Semko on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
